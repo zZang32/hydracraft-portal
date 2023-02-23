5c51a0c620e40081169f377d5d74b4b6b727832c
projects <rbangelo1103@gmail.com>
2023-02-23 22:00:48 +0000

    toma arreglao tonto

--- hydracraft-web/src/App.jsx
+++ hydracraft-web/src/App.jsx
@@ -1,42 +1,35 @@
-import { useState, useEffect } from 'react';
-import Loader from './components/Loader';
+import { useState, useEffect } from "react";
+import Loader from "./components/Loader";
 import "./assets/css/style.css";
-import logo from "./assets/img/hydralogo.jpg";
+import banner from './assets/img/bannerhydra.png';
 
 function App() {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
   const [bar, setBar] = useState(0);
-
 
   useEffect(() => {
     if (bar < 100) {
-      setTimeout(() => setBar(bar + 10), 100)
+      setTimeout(() => setBar(bar + 10), 100);
     }
     if (bar >= 100) {
-      setTimeout(() => setLoaded(true), 1500)
+      setTimeout(() => setLoaded(true), 1500);
     }
   }, [bar, setBar]);
 
-  if (true) return (
-    <div className="App">
-      <div className="d-flex vh-100 aligns-items-center justify-content-center text-center fondo">
-
-          <h2 className="banner-mc">HYDRACRAFT</h2>
-          <h5 className="network">NETWORK</h5>
-
-        <Loader bar={bar} />
+  if (true)
+    return (
+      <div className="App">
+        <div className="d-flex vh-100 flex-column aligns-items-center justify-content-center text-center fondo">
+          <div className="container">
+            <img src={banner} alt="banner" className='img-fluid' />
+            <br /><br />
+            <Loader bar={bar} />
+          </div>
+        </div>
       </div>
+    );
 
-    </div>
-  )
-
-  return (
-    <div className="App">
-
-    </div>
-  )
-
-
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
--- hydracraft-web/src/components/Loader/index.jsx
+++ hydracraft-web/src/components/Loader/index.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
-import "./styles.css"
+import "./styles.css";
 export default function Loader({ bar }) {
-
-    return (
-        <>
-            
-                <ProgressBar completed={bar}
-                    isLabelVisible={false}
-                    maxCompleted={100}
-                    borderRadius={"0"}
-                    barContainerClassName="bar-container"
-                    bgColor="#e9e9e9"
-                    className="m-5"
-                />
-
-        </>
-    );
+  return (
+    <>
+      <ProgressBar
+        completed={bar}
+        isLabelVisible={false}
+        maxCompleted={100}
+        borderRadius={"0"}
+        barContainerClassName="bar-container"
+        bgColor="#e9e9e9"
+        // className="m-5"
+      />
+    </>
+  );
 }
